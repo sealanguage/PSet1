19dd878086d876ac7d6f1ccf5d1f5e2ab87ebe39
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-04-09 01:16:14 +0000

    7 versions of mario, none working. mario7 getting close

---
+++ cash.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+
+}
+
+//  creating a greedy algorithm, a globally optimized solution
+    //  get amount in dollars
+    //  while (quarters can be used)
+    //     increase count
+    //     decrease amount by .25
+    // while (dimes can be used)
+    //     increase count
+    //     decrease amount by .10
+    // while (nickles can be used)
+    //     increase count
+    //     decrease amount by .5
+    // while (pennies can be used)
+    //     increase count
+
+    // print number of coins used
