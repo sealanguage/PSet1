7cc484c99c1d01c348ccff850ad48d15623d439e
Carina Cruz <lecruz@bu.edu>
2018-04-10 23:22:41 +0000

    pr-carina

--- water.c
+++ water.c
@@ -14,3 +14,14 @@ int main(void)
     //printf("%i\n", minutes);
 
 }
+
+// Comments:
+// Hi Elaine. Your code is very concise, which is really great! My only suggestion is adding to the code the condition that the user cannot input a negative number.
+// While testing I noticed that negative numbers can be inputed, which for the task is not nessesary as people cannot have a negative amount of water/bottles.
+
+
+
+
+
+
+
