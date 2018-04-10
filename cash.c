f6c4d22d5a5bdad96b7433215377eeef5ef7654c
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-04-10 22:35:09 +0000

    updates to all files. cash not finished

--- cash.c
+++ cash.c
@@ -1,8 +1,46 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
+    // prompt user for amount in dollars, validate amount, dollars, use get_float
+    //convert dollar to cents * 100   round
+    float change;
+    float changeOwed;
+    int coins = 0;
+    int quarter = 25;
+    // int dime = 10;
+    // int nickle = 5;
+    // int pennie = 1;
+    //int change = get_int("Change: ");
+
+    do
+    {
+        // get amount in dollars
+        change = get_float("Change owed: ");
+        changeOwed = change * 100;
+        printf("reading change owed value%f0\n", changeOwed);
+
+    }
+    while (changeOwed > 0);// just accept positive values
+    printf("storing variable %.0f\n", changeOwed);
+    printf("quarter: ");
+
+        //  while (quarters can be used)
+        for (int i = 0; i > quarter; i++)
+            // for (int j = 0; j >= 25; j++)
+            {
+                 //  increase count
+            coins = coins + 1;
+            printf("reading quarters%d\n", i);
+            //  decrease amount by .25
+            changeOwed = (changeOwed - quarter);
+            printf("changeOwed: %f\n", changeOwed);
+
+            }
+
+
 
 
 }
--- imprecision.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("%5f\n", 1.0 / 10.0);
-}
